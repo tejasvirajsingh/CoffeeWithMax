6b90334a3ea27a3dfb4fab31ea2c1f9e4497e5d8
Tejasvi Raj Singh <tejasvirajsingh2003@gmail.com>
2026-01-18 15:50:41 +0530

    Predefine the color values

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,18 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react() , tailwindcss()] ,
+  plugins:
+   [
+    react() ,
+     tailwindcss(
+      {
+      theme: {
+        color: {
+          CoffeeYellow: "#f4c542",
+          coffeeBrown: "#5c2e05",
+          bgCream: "#fff8e7",
+        },
+      },
+     },
+    ) ] ,
 })
