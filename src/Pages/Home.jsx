97a5190fcb69390f83d947c39755130e29f8d8bf
Tejasvi Raj Singh <tejasvirajsingh2003@gmail.com>
2026-01-18 15:53:00 +0530

    Add Home page and Login page

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,31 @@
+import Navbar from '../Components/Navbar'
+import Hero from '../Components/Hero'
+import HowItWorks from '../Components/HowItWorks'
+import Features from '../Components/Features'
+import Testimonials from '../Components/Testimonials'
+import CallToAction from '../Components/CallToAction'
+import Publish from '../Components/Publish'
+import Footer from '../Components/Footer'
+
+
+
+const Home = () => {
+  return (
+    <div>
+  
+
+
+      <Navbar />
+      <Hero />
+      <HowItWorks />
+      <Features />
+      <Publish />
+      <Testimonials />
+      <CallToAction />
+      <Footer />
+
+    </div>
+  )
+};
+
+export default Home
