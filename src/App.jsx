40b040c429de86f2ad2eab785ec5c7a589bc2fe7
Tejasvi Raj Singh <tejasvirajsingh2003@gmail.com>
2026-01-18 15:52:14 +0530

    fix the error

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,29 @@
 
-import './App.css'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Home from "./Pages/Home";
+import Login from "./Pages/Login";
+import Signup from "./Pages/Signup";
+
+
+
 
 function App() {
   
   return (
-    <>
+    <Router>
+ 
+
+     
+      <Routes>
+        <Route path="/" element= {<Home />} />
+        <Route path= "/Login" element={<Login />} />
+        <Route path = "/Signup" element= {<Signup />} />
+        
+      </Routes>
       
-    </>
+    </Router>
   )
 }
 
-export default App
+export default App;
