5083131a5904edfe7a9714fe9a50e1e6cd186697
Tejasvi Raj Singh <tejasvirajsingh2003@gmail.com>
2026-01-21 21:40:23 +0530

    Create sign up pages

--- src/Pages/Signup.jsx
+++ src/Pages/Signup.jsx
@@ -43,13 +43,67 @@ const Signup = () => {
            </div>
 
                 {/* making bottom section  */}
-                 <div>
+                 <div className="flex flex-col items-center mt-8">
 
-                  <div>
+                  <div className="flex space-x-[-10px]">
+                    {[ "women/1", "men/2", "women/3", "women/4", "men/5" ].map((src,i) => (
+                      <img key={i} 
+                      src={`https://randomuser.me/api/portraits/${src}.jpg`}
+                      alt="userLogo"
+                      className=" w-8 h-8 rounded-full border-2 border-white " 
+                      />
+                    ))}
 
                     
                   </div>
+                  <p className="text-sm text-gray-900 mt-2 font-medium text-center">Loved by 1M + Creators</p>
                  </div>
+      </div>
+
+      {/* Right section Making form here  */}
+
+      <div className="w-full md:w-[70%] bg-white rounded-none md:rounded-l-4xl shadow-xl flex flex-col px-4 py-6 md:px-10 md:py-10">
+
+
+
+        {/* making top bar of Signup pages */}
+         <div className="flex justify-between items-center mb-6 md:mb-10">
+          <Link to="/">
+          <img src={logo} alt={logo} className="w-8 h-8 md:hidden " />
+          </Link>
+          <p className="text-sm text-gray-700">
+            Already Have an Account?{" "}
+            <Link to="/login" className="text-black font-medium underline" >
+            Sign in</Link>
+          </p>
+         </div>
+ {/* making center part of singup page so sign up function will Execuite  */}
+
+        <div className="flex-1 items-center flex   justify-center">
+           
+           <div className="w-full  max-w-xs md:max-w-sm">
+            <h2 className="text-3xl md:text-4xl font-bold text-gray-800 mb-4 text-center">Create Your Account</h2>
+            <p className=" text-sm text-gray-500 mb-4 text-center">Choose a usrname for your page</p>
+
+             {/* Donmain Input Section  */}
+               
+               <div className="flex items-center border rounded-lg overflow-hidden bg-gray-100 mb-6">
+                <span className="px-3 text-gray-600 text-sm font-bold whitespace-nowrap">coffewithmax.com</span>
+                <input type="text" placeholder="enter your name here" className="flex-1 bg-transparent px-2 py-2 outline-none text-gray-800 text-sm"></input>
+               </div>
+               <button className="w-full bg-coffeeBrown hover:bg-opacity-90 text-white font-semibold py-2 rounded-lg transition text-sm mb-3 ">Sign up</button>
+               <p className="text-xs text-gray-500 text-center" >By continuing, you agree to { " "} 
+                <a href="#" className="underline" >terms of Servies</a> {" "} and { " "}
+                <a href="#" className="underline">privacy policy</a>.
+
+               </p>
+
+           </div>
+
+
+
+        </div>
+
       </div>
     </div>
   )
