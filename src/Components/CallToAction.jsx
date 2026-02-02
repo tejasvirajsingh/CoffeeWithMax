e249198f423e6aa10c3d45110baeae2a57db03cb
Tejasvi Raj Singh <tejasvirajsingh2003@gmail.com>
2026-02-02 16:04:29 +0530

    fix

--- src/Components/CallToAction.jsx
+++ src/Components/CallToAction.jsx
@@ -1,9 +1,37 @@
 const CallToAction = () => {
   return (
-    <div>
-      <h2>Call To Action</h2>
-    </div>
-  )
-}
+    <section className="bg-[#f5f1ee] py-2 px-6 text-center sm:px-12">
+      {/* sub heading */}
 
-export default CallToAction
+      <p className="uppercase text-xs tracking-widest text-gray-500 mb-2 font-semibold ">
+        Start Your journey.
+      </p>
+
+      {/* main heading  */}
+
+      <h2 className="text-3xl sm:text-5xl font-extrabold  text-coffeeBrown  mb-4">
+        {" "}
+        Get Support
+      </h2>
+
+      {/* Description  */}
+
+      <p className="max-w-2xl mx-auto text-gray-600 text-xs px-8 sm:text-lg mb-8 ">
+        Join thousand of creator using Ek cup Coffee with max to grow their
+        Audiance , and get connect to Community.
+      </p>
+
+      {/* creating button  */}
+
+      <a
+        href="/signup"
+        className="inline-block bg-coffeeBrown text-white font-bold text-lg sm:text-xl px-10 sm:px-12 py-4 sm:py-5 
+      rounded-full border-2 border-[#BB591C] shadow-[7px_5px_56px_-20px_#BB591C] transition-transform hover:scale-105 duration-300"
+      >
+        Create Your Page
+      </a>
+    </section>
+  );
+};
+
+export default CallToAction;
--- src/Components/Publish.jsx
+++ src/Components/Publish.jsx
@@ -1,8 +1,32 @@
+import publishImage from "../assets/publish.png";
+
+
 const Publish = () => {
   return (
-    <div>
-      <h2>Publish Section</h2>
-    </div>
+    <section className="bg-[#f5f1ee] py-2 px-6 text-center sm:px-12">
+     
+     {/* sub heading */}
+
+     <p className="uppercase text-xs tracking-widest text-gray-500 mb-2 font-semibold ">Content , Audio and Stories.</p>
+
+     {/* main heading  */}
+
+     <h2 className="text-3xl sm:text-5xl font-extrabold  text-coffeeBrown  mb-4"> Start Publishing</h2>
+
+     {/* Description  */}
+
+     <p className="max-w-2xl mx-auto text-gray-600 text-xs px-8 sm:text-lg mb-8 ">Ek cup coffee help to creator publish authentic stories , behind-the-scenes moment, </p>
+
+
+     {/* final imgae  */}
+
+     <div className="max-w-182.5 mx-auto">
+      <img src={publishImage} className="w-full h-auto rounded-xl " />
+     </div>
+
+
+
+    </section>
   )
 }
 
