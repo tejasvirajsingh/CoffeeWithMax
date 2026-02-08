70cfd53a89eb46b1e21767adcf45bae82e6034a5
Tejasvi Raj Singh <tejasvirajsingh2003@gmail.com>
2026-02-09 00:28:55 +0530

    fix all Size and color

--- src/Pages/Signup.jsx
+++ src/Pages/Signup.jsx
@@ -73,7 +73,7 @@ const Signup = () => {
           </Link>
           <p className="text-sm text-gray-700">
             Already Have an Account?{" "}
-            <Link to="/login" className="text-black font-medium underline" >
+            <Link to="/login" className="text-black cursor-pointer font-medium underline" >
             Sign in</Link>
           </p>
          </div>
@@ -88,13 +88,13 @@ const Signup = () => {
              {/* Donmain Input Section  */}
                
                <div className="flex items-center border rounded-lg overflow-hidden bg-gray-100 mb-6">
-                <span className="px-3 text-gray-600 text-sm font-bold whitespace-nowrap">coffewithmax.com</span>
-                <input type="text" placeholder="enter your name here" className="flex-1 bg-transparent px-2 py-2 outline-none text-gray-800 text-sm"></input>
+                {/* <span className="px-3 text-gray-600 text-sm font-bold whitespace-nowrap">coffewithmax.com</span> */}
+                <input type="text" placeholder="Enter your Email id@.com " className="flex-1 bg-transparent px-2 py-2 outline-none text-gray-800 text-sm"></input>
                </div>
-               <button className="w-full bg-coffeeBrown hover:bg-opacity-90 text-white font-semibold py-2 rounded-lg transition text-sm mb-3 ">Sign up</button>
+               <button className="w-full bg-coffeeBrown hover:bg-opacity-90 text-white font-semibold py-2 rounded-lg cursor-pointer transition text-sm mb-3 ">Sign up</button>
                <p className="text-xs text-gray-500 text-center" >By continuing, you agree to { " "} 
-                <a href="#" className="underline" >terms of Servies</a> {" "} and { " "}
-                <a href="#" className="underline">privacy policy</a>.
+                <a href="#" className=" underline decoration-blue-800" >terms of Servies</a> {" "} and { " "}
+                <a href="#" className="underline  decoration-blue-800">privacy policy</a>.
 
                </p>
 
